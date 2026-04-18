251b75cf2f0144b1c0750da2b55e9446bae5e2d7
M. Ditto Putra Pratama <ditoputra111@gmail.com>
2026-04-18 10:14:21 +0700

    Fix DatabaseSeeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,29 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
 use App\Models\Quest;
 use App\Models\Puzzle;
 use App\Models\ShopItem;
 
-public function run(): void
+class DatabaseSeeder extends Seeder
 {
-    $quest = Quest::create([
-        'title' => 'Intro Puzzle',
-        'description' => 'Latihan dasar logika',
-        'reward_points' => 100,
-        'reward_xp' => 50,
-        'order' => 1
-    ]);
+    public function run(): void
+    {
+        $quest = Quest::create([
+            'title' => 'Intro Puzzle',
+            'description' => 'Latihan dasar logika',
+            'reward_points' => 100,
+            'reward_xp' => 50,
+            'order' => 1,
+            'is_active' => true,
+        ]);
 
-    Puzzle::create([
-        'quest_id' => $quest->id,
-        'question' => '2 + 2 = ?',
-        'answer' => '4',
-        'hint' => 'Ini matematika dasar',
-        'order' => 1
-    ]);
+        Puzzle::create([
+            'quest_id' => $quest->id,
+            'question' => '2 + 2 = ?',
+            'answer' => '4',
+            'hint' => 'Ini matematika dasar',
+            'time_limit' => 60,
+            'bonus_points' => 50,
+            'order' => 1,
+        ]);
 
-    ShopItem::create([
-        'name' => 'Hint +1',
-        'type' => 'hint',
-        'value' => 1,
-        'price' => 20
-    ]);
+        ShopItem::create([
+            'name' => 'Hint +1',
+            'type' => 'hint',
+            'value' => 1,
+            'price' => 20,
+        ]);
+    }
 }
